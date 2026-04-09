f119c2de7cc26b3d44eeb460e1a0c70e03d801ff
Anita Raskov <43094115+Anita9@users.noreply.github.com>
2026-04-09 22:06:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,13 +28,6 @@ const App = () => {
       return;
     }
 
-    if (isWordCorrect) {
-      setIsWordIncorrect(false);
-    } 
-    else {
-      setIsWordIncorrect(true);
-    }
-
     const correctWord = isCorrectWord(currentGuessedWord);
 
     if(getWordArrayLength(currentGuessedWord) === MAX_WORD_LENGTH && guessedWords.length < MAX_ATTEMPTS && !isWordCorrect) {
